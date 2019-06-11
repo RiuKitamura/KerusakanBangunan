75b2084e8dc1df515b2472de048979b35e08ab44
Riu Kitamura <habibakbar360@gmail.com>
2019-06-11 22:57:27 +0800

    berhasil mengambil gambar

--- app/src/main/java/com/example/acer/kerusakanbangunan/FormActivity.java
+++ app/src/main/java/com/example/acer/kerusakanbangunan/FormActivity.java
@@ -1,17 +1,23 @@
 package com.example.acer.kerusakanbangunan;
 
-import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
@@ -20,8 +26,14 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.theartofdev.edmodo.cropper.CropImage;
-import com.theartofdev.edmodo.cropper.CropImageView;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class FormActivity extends AppCompatActivity {
 
@@ -29,7 +41,10 @@ public class FormActivity extends AppCompatActivity {
     ImageView poto;
     Button next;
 
-    final int REQUEST_CODE_GALERY = 999;
+    private String cameraFilePath;
+    final int CAMERA_REQUEST_CODE1 = 999;
+    final int CAMERA_REQUEST_CODE2 = 99;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,11 +68,7 @@ public class FormActivity extends AppCompatActivity {
         poto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ActivityCompat.requestPermissions(
-                        FormActivity.this,
-                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                        REQUEST_CODE_GALERY
-                );
+                selectImage();
             }
         });
 
@@ -70,41 +81,102 @@ public class FormActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == REQUEST_CODE_GALERY){
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                galleryIntent.setType("image/*");
-                startActivityForResult(galleryIntent, REQUEST_CODE_GALERY);
-            }
-            else{
-                Toast.makeText(this, "Tidak ada hak permisi mengakses file", Toast.LENGTH_SHORT).show();
-            }
-            return;
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds options to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
+    private void selectImage() {
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(FormActivity.this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Take Photo"))
+                {
+                    captureFromCamera();
+                }
+                else if (options[item].equals("Choose from Gallery"))
+                {
+                    pickFromGallery();
+                }
+                else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private void pickFromGallery(){
+        //Create an Intent with action as ACTION_PICK
+        Intent intent=new Intent(Intent.ACTION_PICK);
+        // Sets the type as image/*. This ensures only components of type image are selected
+        intent.setType("image/*");
+        //We pass an extra array with the accepted mime types. This will ensure only components with these MIME types as targeted.
+        String[] mimeTypes = {"image/jpeg", "image/png"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES,mimeTypes);
+        // Launching the Intent
+        startActivityForResult(intent,CAMERA_REQUEST_CODE2);
+    }
+
+    private void captureFromCamera() {
+        try {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".provider", createImageFile()));
+            startActivityForResult(intent, CAMERA_REQUEST_CODE1);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @SuppressLint("LongLogTag")
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == REQUEST_CODE_GALERY && requestCode == RESULT_OK) {
-            Uri imageUri = data.getData();
-            CropImage.activity(imageUri)
-                    .setGuidelines(CropImageView.Guidelines.ON)
-                    .setAspectRatio(1,1)
-                    .start(this);
-        }
-        if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
-            CropImage.ActivityResult result = CropImage.getActivityResult(data);
-            if(requestCode == RESULT_OK){
-                Uri resultUri = result.getUri();
-                poto.setImageURI(resultUri);
-            }
-            else if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE){
-                Exception error = result.getError();
-            }
-        }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+                if (resultCode == Activity.RESULT_OK)
+                    switch (requestCode){
+                        case CAMERA_REQUEST_CODE1:
+                            poto.setImageURI(Uri.parse(cameraFilePath));
+                            break;
+                        case CAMERA_REQUEST_CODE2:
+                            //data.getData return the content URI for the selected Image
+                            Uri selectedImage = data.getData();
+                            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                            // Get the cursor
+                            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                            // Move to first row
+                            cursor.moveToFirst();
+                            //Get the column index of MediaStore.Images.Media.DATA
+                            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                            //Gets the String value in the column
+                            String imgDecodableString = cursor.getString(columnIndex);
+                            cursor.close();
+                            // Set the Image in ImageView after decoding the String
+                            poto.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString));
+                            break;
+
+                    }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        //This is the directory in which the file will be created. This is the default location of Camera photos
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DCIM), "Camera");
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        // Save a file: path for using again
+        cameraFilePath = "file://" + image.getAbsolutePath();
+        return image;
     }
 
     @Override
